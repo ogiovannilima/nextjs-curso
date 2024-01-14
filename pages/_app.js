c0150bd347104402b65df46b1f4dab7bd665a1c3
Giovanni Lima <giovannilimadev@gmail.com>
2024-01-14 13:56:04 -0300

    Integração com CSS #01 - foi adicionado um arquivo css de importacao global

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import './css/integracao1.css' //importar o css aqui faz com que funcione de forma global
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,9 @@
+export default function integracao() {
+  return (
+    <div className="integracao">
+      <div className="vermelho">TEXTO #01</div>
+      <div className="azul">TEXTO #02</div>
+      <div className="branco">TEXTO #03</div>
+    </div>
+  )
+}
