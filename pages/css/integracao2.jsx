dae4cc5b0749917426a4278543e8aadc9521693a
Giovanni Lima <giovannilimadev@gmail.com>
2024-01-23 10:07:10 -0300

    integracao de css utilizando module para deixar o css scopado

---
+++ pages/css/integracao2.jsx
@@ -0,0 +1,11 @@
+import styles from "./integracao2.module.css"
+
+export default function integracao2() {
+  return (
+    <div id={styles.integracao2}>
+      <div className={styles.vermelho}>TEXTO #01</div>
+      <div className={styles.azul}>TEXTO #02</div>
+      <div className={styles.branco}>TEXTO #03</div>
+    </div>
+  )
+}
