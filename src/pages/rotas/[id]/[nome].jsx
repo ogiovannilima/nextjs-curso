bab93f830efd68d2d39715dceaf3e1085432e4bf
Giovanni Lima <giovannilimadev@gmail.com>
2024-01-24 18:17:48 -0300

    utilizando rotas com parametros pasta e arquivo

---
+++ src/pages/rotas/[id]/[nome].jsx
@@ -0,0 +1,20 @@
+//é possivel acessar mais de um parametro
+// neste caso estamos recebendo o id do primeiro parametro que foi ordenado no nome da pasta
+// e temos o nome do arquivo entre [] que possibilita resgatar o proximo parametro conforme url
+//EX: http://localhost:3000/rotas/33/giovanni
+import { useRouter } from 'next/router'
+
+export default function Buscar() {
+  //dessa forma podemos acessar a informacao que foi enviada via url
+  const router = useRouter()
+  const id = router.query.id
+  const nome = router.query.nome
+  console.log(router)
+
+  return (
+    <div>
+      <h1>Rotas / Id / Buscar!</h1>
+      <h2>Rotas / {id} / {nome}</h2>
+    </div>
+  )
+}
---
+++ src/pages/rotas/[id]/buscar.jsx
@@ -0,0 +1,19 @@
+//colocando o nome de uma pasta dentro de []
+//EX: rotas/[id]/buscar.jsx
+//sera criado uma rota dinamica, onde eu posso passar alguma informacao para dentro do arquivo
+//via url
+import { useRouter } from 'next/router'
+
+export default function Buscar() {
+  //dessa forma podemos acessar a informacao que foi enviada via url
+  const router = useRouter()
+  const id = router.query.id
+  console.log(router)
+
+  return (
+    <div>
+      <h1>Rotas / Id / Buscar!</h1>
+      <h2>Rotas / {id} / Buscar!</h2>
+    </div>
+  )
+}
