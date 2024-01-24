8b68d9309abde4e368ab93581c8ad195cf9c726c
Giovanni Lima <giovannilimadev@gmail.com>
2024-01-24 18:06:49 -0300

    rotas sobre a pasta pages

---
+++ src/pages/_outraRota.jsx
@@ -0,0 +1,8 @@
+//letras maisculas e minusculas sao entendidas na hora de chamar a url
+export default function _OutraRota() {
+  return (
+    <div>
+      <h1>_Outra Rota</h1>
+    </div>
+  )
+} 
---
+++ src/pages/rotas/index.jsx
@@ -0,0 +1,11 @@
+//se for criado arquivos com nome index dentro de alguma pasta
+//e a rota da pasta for chamada
+//EX: localhost:3000/rotas
+//o arquivo index que foi criado sera chamado por padrao
+export default function rotas() {
+  return (
+    <div>
+      <h1>Rotas Index</h1>
+    </div>
+  )
+}
