fd2c24e89f719695dc771ce0a1b7ac40ae66fdd9
Giovanni Lima <giovannilimadev@gmail.com>
2024-01-24 17:58:19 -0300

    estrutura da aplicacao e funcionamento da pasta pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  //pode ser criado variaveis de ambiente aqui dentro
+  env: {
+    dbPath: 'mySql....',
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,3 +5,5 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+//o _app é somente para evitar conflito de nomes
+//ele nao é um arquivo privado, e nenhum arquivo criado com "_" sera privado
